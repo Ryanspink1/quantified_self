5740b734a0f5a9605357dfa430001084fb5770c5
Ryan Spink <ryanspinktech@gmail.com>
2017-05-04 15:29:19 -0600

    server running root endpoint

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.set('port', process.env.PORT || 3000)
-app.local.title = 'Quantified Self'
+app.locals.title = 'Quantified Self'
 
 app.get('/', (request, response) => {
   response.send('Welcome to Quantified Self!')
